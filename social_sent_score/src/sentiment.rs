ef65b5f435c07ba6422f5860d6eb6680f7a7a689
CMBorjas <118933346+CMBorjas@users.noreply.github.com>
2025-04-07 22:52:23 -0600

    Updated to make sure the csv is parsed correctly

--- social_sent_score/src/sentiment.rs
+++ social_sent_score/src/sentiment.rs
@@ -4,21 +4,19 @@ use std::collections::HashMap;
 use std::fs;
 
 pub fn build_social_sentiment_table(file_path: &str) -> HashMap<String, f64> {
-    // Create a HashMap to store the sentiment scores
     let mut sentiment_table = HashMap::new();
-    // Read the sentiment file line by line
+
     if let Ok(contents) = fs::read_to_string(file_path) {
-        // Split the contents into lines and process each line
-        for line in contents.lines() {
-            // Split each line by comma and trim whitespace
+        for (i, line) in contents.lines().enumerate() {
+            // Skip header
+            if i == 0 {
+                continue;
+            }
+
             let parts: Vec<&str> = line.split(',').collect();
-            // Check if the line has exactly two parts: word and score
-            if parts.len() == 2 {
-                // Trim whitespace and parse the score as f64
-                let word = parts[0].trim().to_string();
-                // Check if the score can be parsed as f64
+            if parts.len() >= 2 {
+                let word = parts[0].trim().to_lowercase();
                 if let Ok(score) = parts[1].trim().parse::<f64>() {
-                    // Insert the word and score into the HashMap
                     sentiment_table.insert(word, score);
                 }
             }
@@ -26,9 +24,10 @@ pub fn build_social_sentiment_table(file_path: &str) -> HashMap<String, f64> {
     } else {
         eprintln!("Failed to read sentiment file: {}", file_path);
     }
-    // Return the HashMap containing the sentiment scores
+
     sentiment_table
 }
+
 pub fn get_social_sentiment_score(
     // Path to the review file
     review_file: &str,
---
+++ social_sent_score/target/debug/deps/social_sent_score.d
@@ -0,0 +1,6 @@
+C:\Users\Christian\OneDrive - The University of Colorado Denver\Current Classes\CSCI_3415_001_Principles_of_Programming\003_Homework\Homework_004\social_sent_score\target\debug\deps\social_sent_score.exe: src\main.rs src\sentiment.rs
+
+C:\Users\Christian\OneDrive - The University of Colorado Denver\Current Classes\CSCI_3415_001_Principles_of_Programming\003_Homework\Homework_004\social_sent_score\target\debug\deps\social_sent_score.d: src\main.rs src\sentiment.rs
+
+src\main.rs:
+src\sentiment.rs:
